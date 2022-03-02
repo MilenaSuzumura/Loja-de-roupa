af0b2a0086af12623889eb74ab7985ae1ea68337
Milena <milenaayume@hotmail.com>
2022-03-02 18:10:45 -0300

    adicionei os arquivos

---
+++ codigo/teste.java
@@ -0,0 +1,17 @@
+
+public class teste{
+    public static void main (string[] args){
+        //Variaveis
+        string roupa;
+        int quantidade;
+        string cor;
+    
+        //Menu
+        while(true){
+            System.out.println("---------------------");
+            System.out.println("Escolha uma opção: ");
+            System.out.println("\n1 - Realizar a compra\n2 - Cadastrar cliente;\n3 - Cadastrar produto\n4 - Editar produto\n5 - Editar informações do Cliente\n6 - Sair");
+            System.out.println("\nOpção:");
+        }
+    }
+}
